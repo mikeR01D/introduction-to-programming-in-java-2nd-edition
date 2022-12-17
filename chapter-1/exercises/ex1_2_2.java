b3ec77574d89d8e29970be445c20b0e348c58f64
mikeR01D <mickpower397@gmail.com>
2022-12-17 10:56:37 +0100

    add format for chapter 2 to chapter 4

--- chapter-1/exercises/ex1_2_2.java
+++ chapter-1/exercises/ex1_2_2.java
@@ -1,7 +1,17 @@
 public class ex1_2_2
 {
- // read in the command line argument
- //    
+    public static void main(String[] args)
+    {
+    // read in the command line argument
+    double radian = Double.parseDouble(args[0]);
+    
+    // compute the sin and cos using the Math library and store them in their respective variables
+    double sine = Math.sin(radian);
+    double cosine = Math.cos(radian);
+    // sum the square of both variables and output the result
+    double sum_of_squares = Math.pow(sine, 2) + Math.pow(cosine, 2);
+    System.out.println(sum_of_squares);
+    }
 }
 
 // The reason why the final result is not really one is because the calculations are made using 
---
+++ chapter-1/exercises/ex1_2_3.java
@@ -0,0 +1,8 @@
+public class ex1_2_3
+{
+    public static void main(String[] args)
+    {
+        
+    }
+    }
+}
